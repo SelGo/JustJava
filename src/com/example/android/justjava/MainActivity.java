b73185e18f8b20a1747cd8890248d67a3acb523a
Peter Tsampakiouris <tsampakiourispetros@gmail.com>
2015-09-24 17:38:53 +0300

    Changed $10 textview's ID to order_summary_text_view. Made some changes to the displayMessage method.

--- src/com/example/android/justjava/MainActivity.java
+++ src/com/example/android/justjava/MainActivity.java
@@ -77,18 +77,18 @@ public class MainActivity extends Activity {
         * This method displays the given price on the screen.
         */
         private void displayPrice(int number) {
-            TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+            TextView OrderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
 
-            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+            OrderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
         }
 
 	    /**
 	     * This method displays the given text on the screen.
 	     */
 	    private void displayMessage(String message) {
-	        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-	        
-	        priceTextView.setText(message);
+	        TextView OrderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+
+			OrderSummaryTextView.setText(message);
 	    }
 
 }
